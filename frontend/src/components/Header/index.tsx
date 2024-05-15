ea9a16784d1e3b5bd18e43d968a149c27ad81e92
Gabriel <sollero.gms@gmail.com>
2024-05-15 18:17:01 -0300

    feat: create header component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,5 +1,10 @@
 import classes from './Header.module.css';
 
 export default function Header() {
-  return <div className={classes.header}>Header</div>;
+  return (
+    <div className={classes.header}>
+      <h1>~Product name~&nbsp;</h1>
+      <span>a fullstack challenge by Brain.ag</span>
+    </div>
+  );
 }
