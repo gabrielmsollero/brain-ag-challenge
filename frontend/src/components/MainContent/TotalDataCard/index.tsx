be42c79c2cab44c020e510d26c517de47df87311
Gabriel <sollero.gms@gmail.com>
2024-05-16 06:55:42 -0300

    feat: implement TotalDataCard with dummy data

--- frontend/src/components/MainContent/TotalDataCard/index.tsx
+++ frontend/src/components/MainContent/TotalDataCard/index.tsx
@@ -1,5 +1,18 @@
 import Card from '../../UI/Card';
 
+import classes from './TotalDataCard.module.css';
+
 export default function TotalDataCard() {
-  return <Card title="Totalization"></Card>;
+  return (
+    <Card title="Totalization">
+      <div className={classes.body}>
+        <p>
+          <span className={classes.strong}>432</span> farms
+        </p>
+        <p>
+          <span className={classes.strong}>1959.2</span> ha
+        </p>
+      </div>
+    </Card>
+  );
 }
