052fd09686fcd61045044d0cefd57f4a35b3775d
Gabriel <sollero.gms@gmail.com>
2024-05-20 08:44:30 -0300

    feat: add temporary todo list to MainContent

--- frontend/src/components/MainContent/index.tsx
+++ frontend/src/components/MainContent/index.tsx
@@ -1,4 +1,5 @@
 import AddFarmButton from '../UI/AddFarmButton';
+import Card from '../UI/Card';
 import CultureChartCard from './CultureChartCard';
 import FarmsTableCard from './FarmsTableCard';
 import SoilUsageChartCard from './SoilUsageChartCard';
@@ -17,6 +18,24 @@ export default function MainContent() {
       <div style={{ gridColumn: '1/-1' }}>
         <FarmsTableCard />
       </div>
+      <Card title="TODO">
+        <ul>
+          <li>Organizar melhor layout dos cards</li>
+          <li>Tabela: fuzzy search, paginação, ordenação, formatação</li>
+          <li>Tabela: disposição do uso da área no formato de barra</li>
+          <li>Tabela: disposição das culturas usando toasts</li>
+          <li>Contexto (ou redux) de dados</li>
+          <li>Conexão dos componentes ao contexto p buscar dados</li>
+          <li>Responsividade form</li>
+          <li>Fechar modal ao clicar fora</li>
+          <li>Botão para atualizar gráficos</li>
+          <li>Testes automatizados</li>
+          <li>Consertar estilização selects</li>
+          <li>Animations</li>
+          <li>Dark/light themes</li>
+          <li>Traduções</li>
+        </ul>
+      </Card>
       <AddFarmButton></AddFarmButton>
     </main>
   );
